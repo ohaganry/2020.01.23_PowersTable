bf8d40d38cce2429756280786c49c72afc2b5a66
ohaganry <hermits.nature@gmail.com>
2020-01-23 17:01:46 -0500

    end of day push

--- powerstable/Program.cs
+++ powerstable/Program.cs
@@ -6,7 +6,34 @@ namespace powerstable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declare variables
+            int squared;
+            int cubed;
+            int userInput;
+            string temp;
+            bool proceed = false;
+            //begin validation do-while loop
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Give me a whole number up to 20");
+                    temp = Console.ReadLine();
+                    bool num = int.TryParse(temp, out userInput);
+
+                } while (temp == false);
+               
+            } while (proceed == true);
+        }
+        public static int Cubed(int num1)
+        {
+            int cubed = Squared(num1) * num1;
+            return cubed;
+        }
+        public static int Squared(int num1)
+        {
+            int squared = num1 * num1;
+            return squared;
         }
     }
 }
